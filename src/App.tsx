dfa0e35e30abf690c582a8f777090f86e749b8ef
usamamukarram-arbisoft <usama.mukarram@arbisoft.com>
2025-06-04 18:23:28 +0500

    feat: active router state added for  menus

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import Navbar from "./Components/Navbar/navbar";
 import Products from "./Components/Products/Products";
 import { Routes, Route } from "react-router-dom";
 import BookDetail from "./Components/BookDetail/BookDetail";
+import Dashboard from "./Components/Dashboard/Dashboard";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Products />} />
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/books" element={<Products />} />
         <Route path="/books/:id" element={<BookDetail />} />
       </Routes>
     </div>
--- src/Components/Navbar/navbar.tsx
+++ src/Components/Navbar/navbar.tsx
@@ -1,17 +1,23 @@
 import React from "react";
 import "./navbar.css";
+import { Link, useLocation } from "react-router-dom";
 const Navbar = () => {
   const menus = [
-    { name: "Home", link: "#" },
-    { name: "Products", link: "#" },
+    { name: "Home", link: "/" },
+    { name: "Books", link: "/books" },
   ];
+  const location = useLocation().pathname;
+  console.log("🚀 ~ Navbar ~ location:", location);
   return (
     <ul className="nav justify-content-end">
       {menus.map((menu, index) => (
-        <li className="nav-item" key={index}>
-          <a className="nav-link active" aria-current="page" href={menu.link}>
+        <li
+          className={`nav-item ${location === menu.link ? "active" : ""}`}
+          key={index}
+        >
+          <Link className="nav-link active" aria-current="page" to={menu.link}>
             {menu.name}
-          </a>
+          </Link>
         </li>
       ))}
 
